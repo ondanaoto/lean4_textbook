f14991d1666bb39ae3e52fcf012c6ca47b73415b
Ondanaoto <n.onda.go20@gmail.com>
2024-06-05 19:15:49 +0900

    fix NatDef

--- Lean4Textbook/NatDef.lean
+++ Lean4Textbook/NatDef.lean
@@ -1,32 +1,59 @@
-inductive MyNat : Type where
-  -- ゼロは自然数である
-  | zero : MyNat
-  -- 自然数の次の数は自然数である
-  | succ : MyNat → MyNat
+#check Nat
 
-def zero := MyNat.zero
+example : 1 + 1 = 2 := rfl
 
--- 1は0の次の数と定義する
-def one := MyNat.succ MyNat.zero
+inductive Nat' where
+-- 0は自然数とする
+| zero : Nat'
+-- nが自然数ならsucc nも自然数とする
+| succ (n : Nat') : Nat'
 
--- 2は1の次の数と定義する
-def two := MyNat.succ (MyNat.succ MyNat.zero)
+-- 1は0の後者として定義する
+def one := Nat'.succ Nat'.zero
 
--- 自然数の足し算の定義
-def MyNat.add : MyNat → MyNat → MyNat
-  -- 第1項が0なら第2項が足し算の結果ですよ
-  | .zero, k => k
-  -- (nの次の数)とkの足し算は(nとkの足し算)の次の数としましょう
-  | .succ n, k => .succ (MyNat.add n k)
+-- 2は1の後者として定義する
+def two := Nat'.succ one
 
--- + と書いたら上で定義した足し算のことになりますよという呪文
-instance : Add MyNat where
-  add := MyNat.add
+-- 0はどの自然数の後者にもならない
+theorem succ_ne_zero
+(n : Nat')
+: Nat'.succ n ≠ Nat'.zero
+:= Nat'.noConfusion
 
-example : one + one = two := by calc
-  one + one = MyNat.add one one := by rfl -- + を展開した
-          _ = MyNat.add (MyNat.succ MyNat.zero) one := rfl -- 第1項のoneを展開した
-          _ = MyNat.succ (MyNat.add MyNat.zero one) := rfl -- 足し算の定義に従って式変形した
-          _ = MyNat.succ one := rfl -- 足し算は第1項が0なら第2項が足し算の結果になるのだった
-          _ = MyNat.succ (MyNat.succ MyNat.zero) := rfl -- oneを展開した
-          _ = two := rfl -- twoはそういえば0の次の次だったね!
+-- 後者関数は単射である
+theorem succ_inj
+(n m : Nat')
+(h : Nat'.succ n = Nat'.succ m)
+: n = m
+:= by exact Nat'.succ.inj h
+
+-- 数学的帰納法
+theorem ind
+(E : Nat' → Prop)
+(h0 : E Nat'.zero)
+(hk : ∀ k : Nat', E k → E (Nat'.succ k))
+(n : Nat')
+: E n
+:= by
+induction n with
+| zero => exact h0
+| succ n hn => exact hk n hn
+
+-- Leanでは足し算をどうやって定義してるっけ
+#check Nat.add
+
+-- Nat'に足し算を定義する
+def Nat'.add : Nat' → Nat' → Nat'
+| n, .zero => n
+| n, .succ m => .succ (Nat'.add n m)
+
+-- +を使ったらNat'.addを意味していることをLeanに教える
+instance : Add Nat' where add := Nat'.add
+
+theorem one_add_one_eq_two : one + one = two := by
+calc
+  one + one = Nat'.add one one := by rfl -- 足し算の定義を展開
+  _ = Nat'.add one (Nat'.succ Nat'.zero) := by rfl -- 第二引数のoneを展開
+  _ = Nat'.succ (Nat'.add one Nat'.zero) := by rfl -- 足し算の定義の2つ目のケース
+  _ = Nat'.succ one := by rfl -- ()の中身を、足し算の定義の1つ目のケースに基づいて計算。
+  _ = two := by rfl -- twoとはoneの後者として定義していた
