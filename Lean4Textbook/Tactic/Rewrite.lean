ff492d4702147da2b473c5c5d2d8d63e7018342c
Ondanaoto <n.onda.go20@gmail.com>
2024-06-12 12:38:04 +0900

    rw

---
+++ Lean4Textbook/Tactic/Rewrite.lean
@@ -0,0 +1,39 @@
+import Mathlib.Data.Real.Basic
+-- # 概要
+-- rewrite タクティク`rw`の紹介
+-- 詳細は[タクティク逆引きリスト](https://lean-ja.github.io/tactic-cheatsheet/Tactic/Rw.html)の`rw`のsectionをご覧ください。
+-- rwを使った例は[MILのライブコーディングのアーカイブ](https://youtube.com/live/NQEV0HDAjFA?feature=share)にたくさん登場するのでぜひみてね〜！
+
+example (a b c : Nat) (hab: a = b) (hbc: b = c) : a = c := by
+  -- `hab`という仮定を使って結論である`a=c`を書き換える
+  -- `hab`より`a=b`なので、`a=c`の`a`を`b`に書き換える
+  rw [hab]
+  -- `b=c`を示す問題に変化した。
+  -- なら、仮定`hbc`があるのでそれを引っ張ってくればいい！
+  exact hbc
+
+-- mathlibには実装済みの等式があるので、それらを`rw`で利用することもできます！
+#check (mul_comm)
+#check (mul_assoc)
+
+variable (x y z: ℝ)
+-- `∀`の部分を変数とみなし、代入することで等式を得ることができる
+#check (mul_comm x y)
+#check (mul_assoc x y z)
+
+example (a b c : ℝ) : (c * b) * a = b * (a * c) := by
+  rw [mul_comm c b]
+  rw [mul_assoc b c a]
+  rw [mul_comm c a]
+
+-- 引数を与えない場合は、最初に適用可能な部分に一度だけ適用するようになる。
+example (a b c : ℝ) : a * (b * c) = b * (c * a) := by
+  rw [mul_comm]
+  rw [mul_assoc]
+
+-- `←`を用いると、等式の右辺を左辺に書き換えることもできる。
+example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
+  rw [h']
+  rw [← mul_assoc]
+  rw [h]
+  rw [mul_assoc]
